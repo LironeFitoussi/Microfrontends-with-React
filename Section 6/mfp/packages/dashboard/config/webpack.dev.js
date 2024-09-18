48575b70ae03e1a4e8ba85bbc93781e984bb18d3
LironeFitoussi <lironefit@gmail.com>
2024-09-18 19:51:12 +0300

    Refactor code to use dynamic imports for shared modules

--- Section 6/mfp/packages/dashboard/config/webpack.dev.js
+++ Section 6/mfp/packages/dashboard/config/webpack.dev.js
@@ -1,13 +1,11 @@
 const { merge } = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const packageJson = require("../package.json");
 const commonConfig = require("./webpack.common");
 
 const devConfig = {
   mode: "development",
   devServer: {
-    port: 8080,
+    port: 8081,
     historyApiFallback: {
       index: "index.html",
     },
@@ -15,13 +13,6 @@ const devConfig = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./public/index.html",
-    }),
-    new ModuleFederationPlugin({
-      name: "container",
-      remotes: {
-        marketing: "marketing@http://localhost:8081/remoteEntry.js",
-      },
-      shared: packageJson.dependencies,
     }),
   ],
 };
