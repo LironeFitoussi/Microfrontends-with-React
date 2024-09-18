df3ac756147ac9d1ee7fb1554cb27569f441d9ef
LironeFitoussi <lironefit@gmail.com>
2024-09-18 23:42:27 +0300

    Refactor container to include BrowserRouter and Header component

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,18 +2,29 @@ import React from "react";
 
 // Libraries
 import { BrowserRouter } from "react-router-dom";
-
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 // Components
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
 // console.log(marketingMount);
+
+// Generate class name
+const createGenerateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
+
 export default () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
-    </BrowserRouter>
+    <StylesProvider generateClassName={createGenerateClassName}>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -3,17 +3,25 @@ import React from "react";
 
 // Libraries import
 import { Switch, Route, BrowserRouter } from "react-router-dom";
-import { StylesProvider } from "@material-ui/core/styles";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 
 // Components import
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
 
+// Generate class name
+const generateClassName = createGenerateClassName({
+  productionPrefix: "ma",
+});
+
 // Exported function
 export default () => {
   return (
     <div>
-      <StylesProvider>
+      <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
           <Switch>
             <Route exact path="/pricing" component={Pricing} />
