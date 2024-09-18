878eecc2bcd0f6db4af8f7c6d6a487e81f225dfc
LironeFitoussi <lironefit@gmail.com>
2024-09-18 23:13:41 +0300

    Refactor container to include BrowserRouter and Header component

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
+
+// Libraries
+import { BrowserRouter } from "react-router-dom";
+
+// Components
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
 // console.log(marketingMount);
 export default () => {
   return (
-    <div>
-      <h1>Hi there!!!!!</h1>
-      <hr />
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
   );
 };
