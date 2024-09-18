5a924550ec8eb91c7082672b404bf5c0d0147dde
LironeFitoussi <lironefit@gmail.com>
2024-09-18 17:52:20 +0300

    Refactor webpack.config.js to use dynamic imports for shared modules

--- Section 3/ecomm/products/src/index.js
+++ Section 3/ecomm/products/src/index.js
@@ -1,12 +1,1 @@
-import faker from "faker";
-
-let products = "";
-
-for (let i = 0; i < 10; i++) {
-  const name = faker.commerce.productName();
-  // console.log(name);
-  products += `<div>${name}</div>`;
-}
-
-// console.log(products);
-document.querySelector("#dev-products").innerHTML = products;
+import("./bootstrap");
