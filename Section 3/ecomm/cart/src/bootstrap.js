645cbb22a5b56f6cc24e12c37a8067de9411d3c1
LironeFitoussi <lironefit@gmail.com>
2024-09-18 18:18:53 +0300

    Refactor bootstrap.js to use dynamic imports for shared modules

--- Section 3/ecomm/cart/src/bootstrap.js
+++ Section 3/ecomm/cart/src/bootstrap.js
@@ -1,5 +1,19 @@
 import faker from "faker";
 
+const mount = (el) => {
+  const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
+
+  el.innerHTML = cartText;
+};
+
 const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
 
-document.querySelector("#dev-cart").innerHTML = cartText;
+if (process.env.NODE_ENV === "development") {
+  const el = document.querySelector("#dev-cart");
+
+  if (el) {
+    mount(el);
+  }
+}
+
+export { mount };
--- Section 3/ecomm/products/src/bootstrap.js
+++ Section 3/ecomm/products/src/bootstrap.js
@@ -1,12 +1,38 @@
 import faker from "faker";
 
-let products = "";
+const mount = (el) => {
+  let products = "";
 
-for (let i = 0; i < 10; i++) {
-  const name = faker.commerce.productName();
-  // console.log(name);
-  products += `<div>${name}</div>`;
+  for (let i = 0; i < 5; i++) {
+    const name = faker.commerce.productName();
+    // console.log(name);
+    products += `<div>${name}</div>`;
+  }
+
+  // console.log(products);
+  el.innerHTML = products;
+};
+
+// Context/Situation #1
+// We are running this file in development isolation
+// We are using our local index.html file
+// Which DEFINITELY has an element with an id of 'dev-products'
+// We want to immediately render our app into that element
+
+if (process.env.NODE_ENV === "development") {
+  const el = document.querySelector("#dev-products");
+
+  // Assuming our container does not have an element with id of 'dev-products'
+  if (el) {
+    // We are probably running in isolation
+    mount(el);
+  }
 }
 
-// console.log(products);
-document.querySelector("#dev-products").innerHTML = products;
+// Context/Situation #2
+// We are running this file in development or production
+// through the CONTAINER app
+// NO GUARANTEE that an element with an id of 'dev-products' exists
+// We do not want try to immediately render the app
+
+export { mount }; // by exporting mount function, we can use it in container app
